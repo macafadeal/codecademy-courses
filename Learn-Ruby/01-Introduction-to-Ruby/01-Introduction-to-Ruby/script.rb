841654bbcc891bd8b0887ef4332edcb490d93c8e
Mateus Albuquerque <macafadeal@gmail.com>
2024-09-03 13:15:49 +0400

    Working on Codecademy courses & projects.

---
+++ Learn-Ruby/01-Introduction-to-Ruby/01-Introduction-to-Ruby/script.rb
@@ -0,0 +1,67 @@
+# Introduction ro Ruby
+
+# Data Types: Numbers, Strings, Booleans
+my_num = 25 # Add your code here!
+
+my_boolean = true # And here!
+
+my_string = 'Ruby' # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string
+
+# Variables
+my_num = 100
+
+# Write code above this line!
+
+puts my_num
+
+# Math
+puts 100 + 100
+puts 100 - 100
+puts 100 * 100
+puts 100 / 100
+puts 100**10
+puts 100 % 10
+
+# puts' and 'print'
+puts 'Hello World!'
+print 'Goodbye World!'
+
+# The '.length' Method
+puts 'Mateus'.length
+# 6
+
+# The '.reverse' Method
+puts 'Mateus'.reverse
+# suetaM
+
+# '.upcase' & '.downcase'
+puts 'Mateus'.upcase # MATEUS
+puts 'Mateus'.downcase # mateus
+
+# Single-Line Comments
+# I'm a single-line comment!
+
+# Multi-Line Comments
+=begin
+I'm a
+multi-line comment!
+=end
+
+# Naming Conventions
+name = 'Mateus'
+
+# Variables & Data Types
+my_name = 'Mateus'
+my_age = 24
+
+# Math
+puts sum = 13 + 379
+puts product = 923 * 15
+puts quotient = 13209 / 17
+
+# Strings and String Methods
+
